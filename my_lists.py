23fc56ceb663a032a6d9cfadd4060aeff5a1bb66
DEM <daniel@daniel-Inspiron-5558>
2017-03-09 08:48:14 -0500

    first commit

--- my_lists.py
+++ my_lists.py
@@ -1,9 +1,13 @@
+
 class LinkedList:
 
-    def __init__(self):
+    def __init__(self,value=None,nextitem=None):
         """Creates empty LinkedList"""
-        self._value = None
-        self._next_item = None
+        self._value = value
+        self._next_item = nextitem
+        
+    def prepend(self,val):
+        self.__init__(val, LinkedList(self._value, self._next_item))
 
     def append(self, val):
         if self._value is None:
@@ -11,15 +15,108 @@ class LinkedList:
             self._next_item = LinkedList()
         else:
             self._next_item.append(val)
+            
+    def extend(self,linkedlist):
+        for l in linkedlist:
+            self.append(l)
+            
+    def dump(self,*items):
+        for i in items:
+            self.append(i)
 
     def __len__(self):
-        return 0
+        if self._value == None:
+            return 0
+        else:
+            return 1 + len(self._next_item)
 
     def __getitem__(self, index):
-        return None
+        if index == 0:
+            return self._value
+        elif self._next_item != None:
+            return self._next_item[index-1]
+        raise IndexError("Index %r not in 'LinkedList'"%(index))
+        
+    def __setitem__(self, index, val):
+        if index == 0:
+            self._value = val
+        elif self._next_item != None:
+            self._next_item.insert(index-1,val)
+        else:
+            raise IndexError("Index %r not in 'LinkedList'"%(index))
+            
+    def __delitem__(self, index):
+        self.pop(index)
 
     def insert(self, index, val):
-        pass
+        if index == 0:
+            ll = LinkedList()
+            ll._value = self._value
+            ll._next_item = self._next_item
+            self._value = val
+            self._next_item = ll
+        elif self._next_item != None:
+            self._next_item.insert(index-1,val)
+        else:
+            raise IndexError("Index %r not in 'LinkedList'"%(index))
+                
+    def pop(self,index=None):
+        if index == None:
+            return self._pop(len(self)-1)
+        return self._pop(index)
+            
+    def _pop(self,index=0):
+        if index == 0:
+            value = self._value
+            self.__init__(self._next_item._value, self._next_item._next_item)
+            return value
+        elif self._next_item != None:
+            return self._next_item._pop(index-1)
+        raise IndexError("Index %r not in 'LinkedList'"%(index))
+        
+    def remove(self,equivalent): 
+        if self._next_item != None:
+            if equivalent == self._next_item._value:
+                self._next_item = self._next_item._next_item
+            elif equivalent == self._value:
+                self.pop(0)
+            else:
+                self._next_item.remove(equivalent)      
+        else:
+            raise ValueError("There is no element equal to %r in 'LinkedList'"%(equivalent))
+            
+    def reverse(self):
+        reversed_list = LinkedList()
+        for x in self:
+            if x != None:
+                reversed_list.prepend(x)
+        return reversed_list
+            
+    def index(self,equivalent,i=0):
+        if self._next_item != None:
+            if equivalent == self._value:
+                return i+1
+            else:
+                return self._next_item.index(equivalent,i=i+1)
+        raise ValueError("There is no element equal to %r in 'LinkedList'"%(equivalent))
+        
+    def count(self,item,count=0):
+        if self._next_item != None:
+            if item == self._value:
+                count += 1
+            return self._next_item.count(item,count=count)
+        return count
+        
+    def lastitem(self,index=False,i=0): # get the last item without calling len, can get index if True is passed in to args.
+        if self._next_item._value == None:
+            if index == True:
+                return i+1
+            return self._value
+        else:
+            return self._next_item.lastitem(i=i+1,index=index)
+        
+    def __eq__(self, equivalent): # for now, not testing bc I have not fully implemented it yet
+        return repr(self) == repr(equivalent)
 
     def __repr__(self):
         if self._value is None:
--- test_my_lists.py
+++ test_my_lists.py
@@ -1,4 +1,5 @@
 from my_lists import LinkedList
+import pytest
 
 
 def test_append():
@@ -8,3 +9,173 @@ def test_append():
     ll.append(1)
 
     assert repr(ll) == '(5, (3, (1, ())))'
+    
+    
+def test_prepend():
+    ll = LinkedList()
+    ll.append(5)
+    ll.prepend(3)
+    ll.prepend(1)
+    
+    assert repr(ll) == '(1, (3, (5, ())))'
+    
+
+def test_dump():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    
+    assert repr(ll) == '(5, (3, (1, ())))'
+
+
+def test_len():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    
+    assert len(ll) == 3
+    
+    
+def test_getitem():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    
+    assert ll[0] == 5
+    assert ll[1] == 3
+    assert ll[2] == 1
+    
+    
+def test_indexerror_getitem():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+
+    with pytest.raises(IndexError):
+        ll[234]
+        
+        
+def test_pop():
+    ll = LinkedList()
+    ll.dump(5,3,1,0)
+    
+    assert ll.pop(0) == 5
+    assert ll.pop(2) == 0
+    assert ll.pop() == 1
+    
+
+def test_indexerror_pop():
+    ll = LinkedList()
+    ll.dump(5,3,1,0)
+    
+    with pytest.raises(IndexError):
+        ll.pop(234)
+    
+    
+def test_delitem():
+    ll = LinkedList()
+    ll.dump(5,3,1,0)
+    ll2 = LinkedList()
+    ll2.dump(3,1,0)
+    
+    del ll[0]
+    assert ll == ll2
+    
+    
+def test_indexerror_delitem():
+    ll = LinkedList()
+    ll.dump(5,3,1,0)
+
+    with pytest.raises(IndexError):
+        del ll[234]
+        
+
+def test_setitem():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    ll[0] = 2
+    ll[1] = 9
+    ll[2] = 10
+    
+    assert ll[0] == 2
+    assert ll[1] == 9
+    assert ll[2] == 10
+    
+
+def test_indexerror_setitem():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    
+    with pytest.raises(IndexError):
+        ll[430] = None
+    
+
+def test_insert():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    ll.insert(0,0)
+    
+
+def test_indexerror_insert():
+    ll = LinkedList()
+    ll.dump(5,3,1)
+    
+    with pytest.raises(IndexError):
+        ll.insert(6, 2)
+        
+
+def test_remove():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    ll.remove(1)
+    ll.remove(5)
+    
+    ll2 = LinkedList()
+    ll2.dump(4,3,2)
+    
+    assert repr(ll) == repr(ll2)
+    
+    
+def test_valueerror_remove():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    with pytest.raises(ValueError):
+        ll.remove(8)
+    
+def test_index():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    
+    assert ll.index(5) == 1
+    assert ll.index(3) == 3
+    assert ll.index(1) == 5
+    
+    
+def test_valueerror_index():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    
+    with pytest.raises(ValueError):
+        ll.index(100)
+    
+    
+def test_lastitem():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    
+    assert ll.lastitem() == 1
+    assert ll.lastitem(True) == 5
+    
+    
+def test_reverse():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1)
+    
+    ll2 = LinkedList()
+    ll2.dump(1,2,3,4,5)
+    
+    assert repr(ll.reverse()) == repr(ll2)
+    
+
+def test_count():
+    ll = LinkedList()
+    ll.dump(5,4,3,2,1,5,1,2,3,4,5)
+    
+    assert ll.count(1) == 2
+    assert ll.count(5) == 3
